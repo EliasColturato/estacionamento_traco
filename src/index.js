d16ce08e595b44b0eed73443d4912df83ecd2523
EliasColturato <eliasjuniortksj@hotmail.com>
2023-03-18 16:01:30 -0300

    Filtro de carro com o query
    	Adicionado a opção para filtrar um carro passando como query ao
    	link.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { application } from 'express';
 
 const app = express();
 
@@ -8,6 +8,39 @@ app.get('/api/ping', (request, response) => {
   response.send({
     message: 'pong',
   });
+});
+
+//Endpoints Vehicles
+app.get('/api/vehicles', (request, response) => {
+  const { id } = request.query;
+
+  const vehicles = [
+    {
+      id: 1,
+      name: 'Onix 1.4',
+      owner: 'Marcus Vinicius',
+      type: 'car',
+    },
+    {
+      id: 2,
+      name: 'Cobalt Cinza',
+      owner: 'Maria Eduarda',
+      type: 'car',
+    },
+    {
+      id: 3,
+      name: 'Mobi Preto',
+      owner: 'Elias Colturato',
+      type: 'car',
+    },
+  ];
+  //filtra o id passado como query e retorna todos/o carro com o id
+  if (id) {
+    response.send(vehicles.filter(vehicle => vehicle.id == id));
+    return;
+  }
+
+  response.send(vehicles);
 });
 
 app.listen(8000, () => {
