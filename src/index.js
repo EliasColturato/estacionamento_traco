732463a3bfb02b0268984100780d6a00f3c6bc49
EliasColturato <eliasjuniortksj@hotmail.com>
2023-03-19 11:29:41 -0300

    Endpoints
    	Os endpoint dos veículos foram finalizados e os endpoints para
    	as atividades foram criados e já estão funcionando com o banco
    	de dados

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ app.get('/api/ping', (request, response) => {
 });
 
 //Endpoints Vehicles
+
 app.get('/api/vehicles', async (request, response) => {
   // o await só pode ser utilizado aqui adicionando o "async" após o nosso link da api, na nossa "função pai"
   const db = await openDatabase();
@@ -89,6 +90,121 @@ app.delete('/api/vehicles/:id', async (request, response) => {
     id,
     message: `Veículo [${id}] removido com sucesso`,
   });
+});
+
+//Endpoints Activities
+app.post('/api/activities/checkin', async (request, response) => {
+  const { label } = request.body;
+
+  const db = await openDatabase();
+  const vehicle = await db.get(
+    `
+  SELECT * FROM vehicles WHERE label = ?
+  `,
+    [label]
+  );
+  if (vehicle) {
+    const checkinAt = new Date().getTime();
+    const data = await db.run(
+      `
+      INSERT INTO activities (vehicle_id, checkin_at)
+      VALUES (?, ?)
+    `,
+      [vehicle.id, checkinAt]
+    );
+    db.close();
+    response.send({
+      vehicle_id: vehicle.id,
+      checkin_at: checkinAt,
+      message: `Veículo [${vehicle.label}] entrou no estacionamento`,
+    });
+    return;
+  }
+  db.close();
+  response.send({
+    message: `Veículo ${label} não cadastrado`,
+  });
+});
+
+app.put('/api/activities/checkout', async (request, response) => {
+  const { label, price } = request.body;
+
+  const db = await openDatabase();
+  //primeiro precisamos saber se o carro existe no nosso cadastro
+  const vehicle = await db.get(
+    `
+  SELECT * FROM vehicles WHERE label = ?
+  `,
+    [label]
+  );
+  if (vehicle) {
+    //antes de realizar uma put do checkout é necessário saber se o carro está com o checkout em aberto ainda
+    const activityOpen = await db.get(
+      `
+    SELECT *
+      FROM activities
+    WHERE vehicle_id = ?
+      AND checkout_at IS NULL
+    `,
+      [vehicle.id]
+    );
+    //finalizou a verificação
+    if (activityOpen) {
+      const checkoutAt = new Date().getTime();
+      const data = await db.run(
+        `
+        UPDATE activities 
+          SET  checkout_at  = ?,
+               price = ?
+        WHERE id = ?
+               `,
+        [checkoutAt, price, activityOpen.id]
+      );
+      db.close();
+      response.send({
+        vehicle_id: vehicle.id,
+        checkout_at: checkoutAt,
+        price: price,
+        message: `Veículo [${vehicle.label}] saíu do estacionamento`,
+      });
+      return;
+    }
+    response.send({
+      message: `Veículo não realizou nenhum check-in`,
+    });
+    return;
+  }
+  db.close();
+  response.send({
+    message: `Veículo ${label} não cadastrado`,
+  });
+});
+
+app.delete('/api/activities/:id', async (request, response) => {
+  const { id } = request.params;
+  const db = await openDatabase();
+
+  const data = await db.run(
+    `
+    DELETE FROM activities
+    WHERE id = ?
+  `,
+    [id]
+  );
+  db.close();
+  response.send({
+    id,
+    message: `Atividade ${id} removida com sucesso`,
+  });
+});
+
+app.get('/api/activities', async (request, response) => {
+  const db = await openDatabase();
+  const activities = await db.all(`
+    SELECT * FROM activities
+  `);
+  db.close();
+  response.send(activities);
 });
 
 app.listen(8000, () => {
